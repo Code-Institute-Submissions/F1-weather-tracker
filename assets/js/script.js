89ee14200538b8b66ac8a26ef9adee797098b814
larkinz <andy_larkin33@hotmail.com>
2021-02-09 14:49:18 +0100

    feat: created a script and added track coordinates

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// enable strict mode
+'use strict';
+
+// track coordinates (latitude & longitude)
+const bahrain__coordinates = `26.032,50.511`;
+const imola__coordinates = `44.3435,11.707`;
+const portimao__coordinates = `37.233,-8.629`;
+const barcelona__coordinates = `41.57,2.26`;
+const monaco__coordinates = `43.735,7.422`;
+const baku__coordinates = `40.373,49.853`;
+const montreal__coordinates = `45.504 -73.524`;
+const castellet__coordinates = `43.251,5.794`;
+const spielberg__coordinates = `47.22,14.762`;
+const silverstone__coordinates = `52.073,-1.01`;
+const budapest__coordinates = `47.582,19.251`;
+const spa__coordinates = `50.442,5.972`;
+const zandvoort__coordinates = `52.388,4.541`;
+const monza__coordinates = `45.624,9.282`;
+const sochi__coordinates = `43.407,39.952`;
+const singapore__coordinates = `1.291,103.864`;
+const suzuka__coordinates = `34.8439,136.5312`;
+const austin__coordinates = `30.1302,-97.637`;
+const mexico__coordinates = `19.406,-99.093`;
+const interlagos__coordinates = `-23.706,-46.699`;
+const melbourne__coordinates = `-37.851, 144.97`;
+const jeddah__coordinates = `21.602,39.108`;
+const UAE__coordinates = `24.467,54.603`;
